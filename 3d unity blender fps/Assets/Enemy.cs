06171f06c504ebe2cf8cb0f9de39de7f7508423b
PodejrzanyS <d.sporek@wp.pl>
2021-11-13 00:49:47 +0100

    guwno

--- 3d unity blender fps/Assets/Enemy.cs
+++ 3d unity blender fps/Assets/Enemy.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float health;
+    Rigidbody rb;
+    GameObject target;
+    float moveSpeed;
+    Vector3 directionToTarget;
+    public GameObject explosion;
     void Start()
     {
-        
+        target = GameObject.Find("Drzewo");
+        rb = GetComponent<Rigidbody>();
+        moveSpeed = Random.Range(1f, 5f);
+        transform.position = new Vector3(transform.position.x + Random.Range(-10, 10), transform.position.y, transform.position.z + Random.Range(-10, 10));
     }
-
     // Update is called once per frame
     void Update()
     {
-        if(health<=0)
+        MoveMonster();
+
+        if (health <= 0)
         {
             Die();
         }
@@ -22,5 +31,21 @@ public class Enemy : MonoBehaviour
     public void Die()
     {
         Destroy(this.gameObject);
+    }
+
+    void MoveMonster()
+    {
+        if (target != null)
+        {
+            directionToTarget = (target.transform.position - transform.position).normalized;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+            transform.LookAt(target.transform.position);
+
+        }
+        else
+        {
+         
+            rb.velocity = Vector3.zero;
+        }
     }
 }
