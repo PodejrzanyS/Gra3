737437ae37d4988fe5b9132a8b3754ab14df93ad
Wolnowar <56628068+Wolnowar@users.noreply.github.com>
2021-11-14 11:43:34 +0100

    zapisywanie coins

--- 3d unity blender fps/Assets/Scripts/PlayerMovement.cs
+++ 3d unity blender fps/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public Text text1;
 
     public float gravity = -9.81f;
 
@@ -21,38 +24,41 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-        // Update is called once per frame
-        void Update()
-        {
+    void Start()
+    {
+        coins = PlayerPrefs.GetInt("coins", 0);
+    }
+    void Update()
+    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
 
-             velocity.y = -2f;
-            
+            velocity.y = -2f;
+
 
         }
 
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-            Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * x + transform.forward * z;
 
-            controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-           
+
         }
 
-            velocity.y += gravity * Time.deltaTime;
+        velocity.y += gravity * Time.deltaTime;
 
-            controller.Move(velocity * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+        text1.text = "Coinsy:" + coins;
 
-        
-        }
+    }
 
     void OnCollisionEnter(Collision other)
     {
@@ -60,6 +66,7 @@ public class PlayerMovement : MonoBehaviour
         {
             int addcoins = Random.Range(1, 5);
             coins = coins + addcoins;
+            PlayerPrefs.SetInt("coins", coins);
             Destroy(other.gameObject);
             Debug.Log("Coinsy:" + coins);
         }
